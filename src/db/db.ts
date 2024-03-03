4afdd0828edeffb6da68c8b7e025bb5ed318ff09
Vinicius Cestari <viniciuscestari01@gmail.com>
2024-03-03 19:52:18 -0300

    fix: use correct host on host mode

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,5 +1,5 @@
 import pg from 'pg'
-const URL = process.env.DB_URL ?? 'postgres://admin:123@db:5432/rinha'
+const URL = process.env.DB_URL ?? 'postgres://admin:123@localhost:5432/rinha'
 
 const pool = new pg.Pool({
     connectionString: URL,
